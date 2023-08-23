81bb8f0f3c8937ff5e45fb9d789ebe9d472ba399
likespike <117564349+likespike@users.noreply.github.com>
2023-08-23 17:07:20 +0900

    validate_results.py

--- controller/validate_results.py
+++ controller/validate_results.py
@@ -1,7 +1,7 @@
 
 import cv2
 import pybboxes as pbx
-# from google.colab.patches import cv2_imshow
+from google.colab.patches import cv2_imshow
 
 def draw_yolo(image, labels):
     H, W = image.shape[:2]    
@@ -11,6 +11,6 @@ def draw_yolo(image, labels):
         cv2.rectangle(image, (box_voc[0], box_voc[1]), 
                     (box_voc[2], box_voc[3]), (0, 0, 255), 1)
     cv2.imwrite("output_vis.png", image)
-    cv2.imshow("output_vis", image)
-    # cv2_imshow(image)    # 코랩 버전으로 변경함
+    # cv2.imshow("output_vis", image)
+    cv2_imshow(image)    # 코랩 버전으로 변경함
     cv2.waitKey(0)
